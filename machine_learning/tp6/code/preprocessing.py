510355d263502c59e61a9c3af2976b472a750f9a
Tanguy ROUDAUT <tanguy@roudaut.xyz>
2023-11-21 14:57:09 +0100

    question 4

--- machine_learning/tp6/code/preprocessing.py
+++ machine_learning/tp6/code/preprocessing.py
@@ -13,13 +13,24 @@ from displayFeatures3d import displayFeatures3d
 def preprocessing():
     img73, img87 = loadImages()
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     plt.imshow(img73)
     plt.title('Image 1973')
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.imshow(img87)
     plt.title('Image 1987')
+
+    img73 = img73[60:-40, :, :]
+    img87 = img87[60:-40, :, :]
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(img73)
+    plt.title('Cropped Image 1973')
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(img87)
+    plt.title('Cropped Image 1987')
 
     plt.show()
 
