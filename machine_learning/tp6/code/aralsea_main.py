a4d8642486bfb0e6bdf9a0c3841694bbecbfb194
Tanguy ROUDAUT <tanguy@roudaut.xyz>
2023-11-21 17:08:38 +0100

    question 7.2

--- machine_learning/tp6/code/aralsea_main.py
+++ machine_learning/tp6/code/aralsea_main.py
@@ -77,8 +77,10 @@ img73_label, img73_plot = displayImageLabel(labels_prediction_img73, img73)
 img87_label, img87_plot = displayImageLabel(labels_prediction_img87, img87)
 
 # Pourcentage de la mer
-proportion_mer_img73 = (np.sum(img73_label == 1) / img73_label.size)*100
-proportion_mer_img87 = (np.sum(img87_label == 1) / img87_label.size)*100
+answer = input('Numero de la classe de la mer ? ')
+cl_mer = int(answer)
+proportion_mer_img73 = (np.sum(img73_label == cl_mer) / img73_label.size)*100
+proportion_mer_img87 = (np.sum(img87_label == cl_mer) / img87_label.size)*100
 
 print("Proportion de la mer dans l'Image 1973:", proportion_mer_img73)
 print("Proportion de la mer dans l'Image 1987:", proportion_mer_img87)
@@ -90,82 +92,42 @@ Proportion de la mer dans l'Image 1987: 21.373769627030974
 Une chute du niveau de la mer de: 6.259059302985111
 """
 
-# ------------------------------------------------
-# YOUR CODE HERE
 
 
+### Approche   non-supervisée  par  GMM
+# Apprentissage de la fonction de classement
+model = unsupervisedTraining(featLearn, method='gmm')
+
+# prediction des labels sur la base d'apprentissage
+labels_predict = model.predict(featLearn)
+
+# Visualisation des resultats
+displayFeatures2d(featLearn, labels_predict)
+displayFeatures3d(featLearn, labels_predict)
 
 
+## Classement et estimation de la diminution de surface
+# Classifying / Predicting / Testing
+labels_prediction_img73 = unsupervisedClassifying(model, img73)
+labels_prediction_img87 = unsupervisedClassifying(model, img87)
 
 
+# mise en forme de l'image de 1973 et 1987 en matrice Num Pixels / Val Pixels
+img73_label, img73_plot = displayImageLabel(labels_prediction_img73, img73)
+img87_label, img87_plot = displayImageLabel(labels_prediction_img87, img87)
 
-
-
-
-
-
-
-
-
-# ------------------------------------------------
-# ------------------------------------------------
-
-
-# Classement des deux jeux de données et visualisation des résultats en image
-
-# ------------------------------------------------
-# YOUR CODE HERE
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ------------------------------------------------
-# ------------------------------------------------
-
-
-
-#%% Estimation de la surface perdue
+# Pourcentage de la mer
 answer = input('Numero de la classe de la mer ? ')
 cl_mer = int(answer)
+proportion_mer_img73 = (np.sum(img73_label == cl_mer) / img73_label.size)*100
+proportion_mer_img87 = (np.sum(img87_label == cl_mer) / img87_label.size)*100
 
+print("Proportion de la mer dans l'Image 1973:", proportion_mer_img73)
+print("Proportion de la mer dans l'Image 1987:", proportion_mer_img87)
+print("Une chute du niveau de la mer de:", proportion_mer_img73-proportion_mer_img87)
 
-
-# ------------------------------------------------
-# YOUR CODE HERE
-
-
-
-
-
-
-
-
-
-
-# ------------------------------------------------
-# ------------------------------------------------
-
-
-
+"""
+Proportion de la mer dans l'Image 1973: 24.335534408428973
+Proportion de la mer dans l'Image 1987: 19.05388782686432
+Une chute du niveau de la mer de: 5.2816465815646545
+"""
